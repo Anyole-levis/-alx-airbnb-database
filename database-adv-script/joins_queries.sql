b79223c099f8d02106ec978d2866bbb70c4c6057
Anyole-levis <levisloprimo@gmail.com>
2025-06-04 13:55:54 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,2 +1,11 @@
-SELECT Bookings 
+SELECT Booking.booking_id, Booking.property_id,
+  Booking.start_date,
+  Booking.end_date, 
+  User.user_id,
+  User.first_name, 
+  User.last_name
+  User.email
+FROM Booking 
+INNER JOIN Booking ON User.user_id = Booking.userid 
+
 
