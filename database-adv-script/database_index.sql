4d18a868ca5bc166c3817d0fe62c5333e617ca9e
Anyole-levis <levisloprimo@gmail.com>
2025-06-05 13:22:29 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,1 +1,4 @@
-
+CREATE INDEX idx_user_user_id ON User(user_id)
+CREATE INDEX idx_booking_id ON Booking(booking_id)
+CREATE INDEX idx_property_id ON Property(property_id)
+CREATE INDEX idx_rating ON Review (ratings)
