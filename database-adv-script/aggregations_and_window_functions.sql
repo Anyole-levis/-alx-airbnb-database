fff8fd281470aa4192ebc318de05cc3355e2f292
Anyole-levis <levisloprimo@gmail.com>
2025-06-05 13:05:00 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -4,5 +4,5 @@ LEFT JOIN Booking ON user.user_id = booking.user_id
 GROUP BY user.user_id, user.first_name, user.last_name
 
 SELECT Booking.property_id, Property.name, RANK() OVER (ORDER BY total_booking DESC) AS RANK,
-  ROW_NUMBER () OVER(ORDER BY total_booking DESC) AS ROW_NUM
+  ROW_NUMBER() OVER(ORDER BY total_booking DESC) AS ROW_NUM
 FROM Booking
