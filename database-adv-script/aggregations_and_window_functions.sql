1d5a1ffe8437d6e0965f1ce58abbdbf02ba9d7c0
Anyole-levis <levisloprimo@gmail.com>
2025-06-05 13:01:04 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,3 +2,6 @@ SELECT user.user_id, user.first_name, user.last_name, COUNT(booking.booking_id) 
 FROM User 
 LEFT JOIN Booking ON user.user_id = booking.user_id
 GROUP BY user.user_id, user.first_name, user.last_name
+
+SELECT Booking.property_id, Property.name, RANK() OVER (ORDER BY total_booking DESC)
+FROM Booking
