1818adb40d8d23ddb78f20bbe992da7a767cb211
Anyole-levis <levisloprimo@gmail.com>
2025-06-05 13:41:28 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -2,3 +2,10 @@ CREATE INDEX idx_user_user_id ON User(user_id)
 CREATE INDEX idx_booking_id ON Booking(booking_id)
 CREATE INDEX idx_property_id ON Property(property_id)
 CREATE INDEX idx_rating ON Review (ratings)
+-- Before adding index
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 42;
+
+-- After adding index
+EXPLAIN ANALYZE
+SELECT * FROM bookings WHERE user_id = 42;
