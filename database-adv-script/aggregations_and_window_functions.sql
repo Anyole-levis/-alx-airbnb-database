5dcf1af06c7e8e8502c1f509b80cf3a6f5217b3f
Anyole-levis <levisloprimo@gmail.com>
2025-06-05 12:40:55 +0300

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,1 +1,4 @@
-
+SELECT user.user_id, user.first_name, user.last_name, COUNT(booking.booking_id) AS total_bookings
+FROM User 
+LEFT JOIN Booking ON user.user_id = booking.user_id
+GROUP BY user.user_id, user.first_name, user.last_name
