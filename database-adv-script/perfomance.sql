daa8963cf353f4025f61edfb8d5c6d9733025a90
Anyole-levis <levisloprimo@gmail.com>
2025-06-06 16:26:30 +0300

    Create  perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,21 @@
+SELECT User.user_id,   Booking.booking_id,
+  Booking.user_id,
+  Booking.start_date,
+  Booking.end_date,
+  Booking.total_price, 
+  Booking.status,
+  Booking.created_at,
+  user.user_id,
+  User.first_name,
+  User.last_name, 
+  Property.property_id,
+  Property.name,
+  Property.location,
+  User.email, 
+  Payment.payment_id,
+  Payment.amount,
+  Payment.payement_date,
+  Payment.Booking_id,
+FROM Booking
+JOIN USER ON Booking.user_id = User.user_id 
+JOIN Property ON Booking.property_id = Property.property_id
